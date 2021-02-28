9c4a7836d49ec65666b5de7935b6c8c19008075d
Ruan Moralez <ruan.moralez@hotmail.com>
2021-02-27 23:51:24 -0300

    update project

---
+++ dist/precache-manifest.0e91afe975938d0ada868de939b8f546.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "b237f4cb74fd7c1a9e87",
+    "url": "/js/chunk-ac180358.63641d3a.js"
+  },
+  {
+    "revision": "06017326ac3feb6477ca5d1e40489e0b",
+    "url": "/img/Vue-Icons.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8ed92807d34f2fd0c13a",
+    "url": "/js/app.52726d01.js"
+  },
+  {
+    "revision": "813faea8640a9cf3ad57de5dae84d425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9505ae0e205f0bb74864",
+    "url": "/js/chunk-383a92c9.0f26be82.js"
+  },
+  {
+    "revision": "06017326ac3feb6477ca5d1e40489e0b",
+    "url": "/img/logo.06017326.png"
+  },
+  {
+    "revision": "20ac3c418cc08d657994",
+    "url": "/js/chunk-3b05bc68.24607266.js"
+  },
+  {
+    "revision": "0de836034aca9796dc5f0251ae53d9ed",
+    "url": "/img/create-account.0de83603.svg"
+  },
+  {
+    "revision": "c904bc80ae6f8f8f5d99",
+    "url": "/js/chunk-3b496cd6.25ea302c.js"
+  },
+  {
+    "revision": "b79e577f6865ae737e7c83cf56d994d4",
+    "url": "/img/login-img.b79e577f.png"
+  },
+  {
+    "revision": "1435fc8a5a397c46a48e",
+    "url": "/js/about.d8833cb5.js"
+  },
+  {
+    "revision": "2df6b79bfa2e88bbe918",
+    "url": "/js/chunk-vendors.16da3ac1.js"
+  },
+  {
+    "revision": "f7c53c872439aaf906d5",
+    "url": "/js/chunk-b720b24e.150e9e3d.js"
+  },
+  {
+    "revision": "2df6b79bfa2e88bbe918",
+    "url": "/css/chunk-vendors.9c887e8e.css"
+  },
+  {
+    "revision": "f7c53c872439aaf906d5",
+    "url": "/css/chunk-b720b24e.f81051f0.css"
+  },
+  {
+    "revision": "b237f4cb74fd7c1a9e87",
+    "url": "/css/chunk-ac180358.a99e4a23.css"
+  },
+  {
+    "revision": "c904bc80ae6f8f8f5d99",
+    "url": "/css/chunk-3b496cd6.a7d30743.css"
+  },
+  {
+    "revision": "20ac3c418cc08d657994",
+    "url": "/css/chunk-3b05bc68.ae09e5fd.css"
+  },
+  {
+    "revision": "9505ae0e205f0bb74864",
+    "url": "/css/chunk-383a92c9.62e41b49.css"
+  },
+  {
+    "revision": "8ed92807d34f2fd0c13a",
+    "url": "/css/app.34e731af.css"
+  },
+  {
+    "revision": "1435fc8a5a397c46a48e",
+    "url": "/css/about.0103099d.css"
+  }
+];
